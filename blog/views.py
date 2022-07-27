7612224380910a08eb4e8a37c6435a90801b8030
Geanderson Esteves <14096071+gesteves91@users.noreply.github.com>
2022-07-26 21:36:33 -0400

    add the form to include a blog post

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
 
 class BlogListView(ListView):
@@ -9,4 +10,7 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
-# Create your views here.
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
